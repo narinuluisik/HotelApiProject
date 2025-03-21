c4a879f379ca3da7827d12a6dd6bf70d20edab61
narin uluışık <narinuluisik7@gmail.com>
2025-03-21 23:04:41 +0300

    Proje dosyası ekle.

---
+++ ApiConsume/HotelProjectDataAccessLayer/EntityFramework/EfStaffDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProjectDataAccessLayer.Abstract;
+using HotelProjectDataAccessLayer.Concrete;
+using HotelProjectDataAccessLayer.Repositories;
+using HotelProjectEntityLayer.Concrete;
+namespace HotelProjectDataAccessLayer.EntityFramework
+{
+    public class EfStaffDal : GenericRepository<Service>, IServicesDal
+    {
+
+        public EfStaffDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
